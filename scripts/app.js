f14652808e9b4b310b286c43f8d1bd82ccaa9a63
Hyunji Kim <69276681+creativehkim@users.noreply.github.com>
2021-07-14 16:52:22 -0400

    adding images

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,16 @@
 const cityForm = document.querySelector('form')
 const card = document.querySelector('.card')
 const details = document.querySelector('.details')
+const time = document.querySelector('img.time')
+const icon = document.querySelector('.icon img')
 
 const updateUI = (data) => {
 
-  const cityDets = data.cityDets
-  const weather = data.weather
+  // const cityDets = data.cityDets
+  // const weather = data.weather
+
+  // destructure properties
+  const { cityDets, weather } = data
 
   // update details template
   details.innerHTML = `
@@ -16,6 +21,17 @@ const updateUI = (data) => {
       <span>&deg;C</span>
     </div>
   `
+
+  // update the night/day & icon images
+
+  let timeSrc = null;
+  if(weather.IsDayTime) {
+    timeSrc = 'img/day.svg'
+  } else {
+    timeSrc = 'img/night.svg'
+  }
+
+time.setAttribute('src', timeSrc)
 
   // remove the d-none class if present
   if(card.classList.contains('d-none')) {
