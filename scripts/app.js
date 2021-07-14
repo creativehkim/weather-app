c8e102f96b47bee59e7b7352707d6f383e337a38
Hyunji Kim <69276681+creativehkim@users.noreply.github.com>
2021-07-14 17:40:49 -0400

    set local storage

--- scripts/app.js
+++ scripts/app.js
@@ -26,13 +26,18 @@ const updateUI = (data) => {
   // update the night/day & icon images
   const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
   icon.setAttribute('src', iconSrc)
+  
+  // let timeSrc = null;
 
-  let timeSrc = null;
-  if(weather.IsDayTime) {
-    timeSrc = 'img/day.svg'
-  } else {
-    timeSrc = 'img/night.svg'
-  }
+  // if(weather.IsDayTime) {
+  //   timeSrc = 'img/day.svg'
+  // } else {
+  //   timeSrc = 'img/night.svg'
+  // }
+
+  // changing the if statement above to a ternary operator
+
+  let timeSrc = weather.IsDayTime ? 'img/day.svg' :'img/night.svg'
 
 time.setAttribute('src', timeSrc)
 
@@ -64,4 +69,7 @@ cityForm.addEventListener('submit', e => {
   updateCity(city)
     .then(data => updateUI(data))
     .catch(err => console.log(err))
+
+  // set local storage
+  localStorage.setItem('city', city)
 })
