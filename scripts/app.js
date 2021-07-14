2ad5a0e426bc5d1eb9c7c3168e7f0895a51bdfc8
Hyunji Kim <69276681+creativehkim@users.noreply.github.com>
2021-07-14 17:09:24 -0400

    added icocns

--- scripts/app.js
+++ scripts/app.js
@@ -22,7 +22,10 @@ const updateUI = (data) => {
     </div>
   `
 
+
   // update the night/day & icon images
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+  icon.setAttribute('src', iconSrc)
 
   let timeSrc = null;
   if(weather.IsDayTime) {
